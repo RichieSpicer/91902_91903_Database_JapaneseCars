4c9beae2c273580db5ff79fb3ac18a1b788739c1
RichieSpicer <Richiespicer@mmc.school.nz>
2026-09-18 09:40:23 +1200

    Delete Japanese Legends1 directory

--- Japanese Legends1/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-unset($_SESSION['admin']);
-unset($_SESSION);
-header("Location: index.php?page=admin/login");
-
-?>
--- Japanese Legends1/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("DB_NAME", "richiespicer_2");
-define("DB_USERNAME", "richiespicer");
-define("DB_PASSWORD", "h330Wu69QLAewW");
-define("DB_HOST", "localhost");
-
-?>
